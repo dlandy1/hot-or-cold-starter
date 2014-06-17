97ab3ebd3207b1a390c880a809aa383913d9e5ef
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-06-17 16:57:19 -0400

    add items

--- js/app.js
+++ js/app.js
@@ -11,7 +11,31 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
+  	var guess = function(value) {
+	  	$("input").keydown(function(event) {
+	  		var value = $("#userGuess").val().trim();
+	  		var item = '<li>'+value+'</li>';
+	  		if (event.keyCode == 13){
+	  		 if (value%1 ==0 && value <= 100 && value.length > 0) {
+	  		 	$( "#guessList").append(item);
+	  		 	$("input").val('');
+	  	}
+	  		else {
+	  		var error = alert("Put in a number 1-100");
+	  		error;
+	  	};
+	  }
+	  })
+	   $("#guessButton").mousedown(function(event){
+	   		var value = $("#userGuess").val().trim();
+	  		var item = '<li>'+value+'</li>';
+	  		if (value%1 ==0){
+	  		 	$( "#guessList").append(item);
+	  		 	$("input").val('');
+	  		 };
+	  	})
+	}
+guess();
 });
 
 
