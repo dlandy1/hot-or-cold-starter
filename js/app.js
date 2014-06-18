58f976341c45024843678cb6a43812694c3db99e
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-06-18 01:26:16 -0400

    numbers

--- js/app.js
+++ js/app.js
@@ -11,31 +11,37 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+  	var numb; 
+  	var creatNumber = function() {
+  		numb= Math.floor((Math.random() * (100))+1);
+  		numb= +numb;
+  	};
+  	creatNumber();
+  	console.log(numb);
   	var guess = function(value) {
-	  	$("input").keydown(function(event) {
 	  		var value = $("#userGuess").val().trim();
 	  		var item = '<li>'+value+'</li>';
-	  		if (event.keyCode == 13){
-	  		 if (value%1 ==0 && value <= 100 && value.length > 0) {
+	  		 if (value%1 ==0 && value <= 100 && value >= 0 && value !== "") {
 	  		 	$( "#guessList").append(item);
 	  		 	$("input").val('');
 	  	}
 	  		else {
 	  		var error = alert("Put in a number 1-100");
 	  		error;
-	  	};
-	  }
-	  })
-	   $("#guessButton").mousedown(function(event){
-	   		var value = $("#userGuess").val().trim();
-	  		var item = '<li>'+value+'</li>';
-	  		if (value%1 ==0){
-	  		 	$( "#guessList").append(item);
-	  		 	$("input").val('');
-	  		 };
-	  	})
-	}
-guess();
+	  		$("input").val('');
+	  	}
+	};
+$("#userGuess").keydown(function(event) {
+	  		if (event.which == 13){
+				guess();
+			}
+		});
+$("#guessForm").submit(function(e) {
+    e.preventDefault();
+  });
+$("#guessButton").mousedown(function(event){
+	guess();
+})
 });
 
 
