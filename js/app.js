d705ec9e7a10315fcce2ed5b00ac708a332a0186
dlandy1 <DanLandy@daniels-mbp-6.babson.edu>
2014-06-19 18:09:41 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -33,25 +33,33 @@ $(document).ready(function(){
   	var guess = function() {
 	  		var value = $("#userGuess").val().trim();
 	  		var item = '<li>'+value+'</li>';
-	  		var difference = value - numb;
+	  		var difference = Math.abs(value - numb);
 	  		 if (value%1 == 0 && value <= 100 && value >= 0 && value !== "") {
 	  		 	$( "#guessList").append(item);
-	  		 	$("#userGuess").val(' ');
 	  		 	trials.push(value);
 	  		 	$("#count").text(trials.length);
-	  		 	if (difference<=1 && difference >= -1 && difference !==0){
+	  		 	if (difference == 1){
 	  		 		$("#feedback").text("Almost on fire!");
 	  		 	}
-	  		 	else if (difference<=2 && difference >= -2 && difference !==0){
+	  		 	else if (difference ==2){
 	  		 		$("#feedback").text("Very Hot");
 	  		 	}
-	  		 	else if (difference<=5 && difference >= -5 && difference !==0){
+	  		 	else if (difference <= 5 && difference !==0){
 	  		 		$("#feedback").text("Hot");
-	  		 	} else if (difference<=10 && difference >= -10 && difference !==0){
+	  		 	} else if (difference<=10 && difference !==0){
 	  		 		$("#feedback").text("Warm");
 	  		 	}
-	  		 	else if (difference<=25 && difference >= -25 && difference !==0){
+	  		 	else if (difference<=25 && difference !==0){
 	  		 		$("#feedback").text("Luke warm");
+	  		 	}
+	  		 	else if (difference<=35 && difference !==0){
+	  		 		$("#feedback").text("Cold");
+	  		 	}
+	  		 	else if (difference<=50 && difference !==0){
+	  		 		$("#feedback").text("Very Cold");
+	  		 	}
+	  		 	else if (difference>50 && difference !==0){
+	  		 		$("#feedback").text("Ice Cold");
 	  		 	}
 	  		 	 else if (value == numb){
 	  		 		  $("#feedback").text("Correct");
@@ -60,12 +68,8 @@ $(document).ready(function(){
 	  		 		  	newGame();
 	  		 		  }
 	  		 	}
-	  		 	else {
-	  		 		$("#feedback").text("Cold");
-	  		 	}
 	  	}
 	  		else {
-	  		$("#userGuess").val(' ');
 	  		var error = alert("Put in a number 1-100");
 	  		error;
 	  	}
@@ -74,11 +78,15 @@ $(document).ready(function(){
 $("#userGuess").keydown(function(event) {
 	  		if (event.which == 13){
 				guess();
+		$("#userGuess").val("").select();
+		return false;
 			}
 		});
 /*--- When guess button is clicked on guess---*/
 $("#guessButton").mousedown(function(event){
 	guess();
+	$("#userGuess").val("").select();
+		return false;
 })
 $(".new").mousedown(function(event){
 	newGame();
