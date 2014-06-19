1831df754c16e6ee09367d1bf60834c1814bfdba
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-06-18 21:34:24 -0400

    Guess

--- js/app.js
+++ js/app.js
@@ -20,11 +20,29 @@ $(document).ready(function(){
   	var guess = function() {
 	  		var value = $("#userGuess").val().trim();
 	  		var item = '<li>'+value+'</li>';
+	  		var difference = value - numb;
 	  		 if (value%1 ==0 && value <= 100 && value >= 0 && value !== "") {
 	  		 	$( "#guessList").append(item);
 	  		 	$("input").val('');
-	  		 	if (value == numb){
+	  		 	if (difference<=1 && difference >= -1 && difference !==0){
+	  		 		$("#feedback").text("Almost on fire!");
+	  		 	}
+	  		 	else if (difference<=2 && difference >= -2 && difference !==0){
+	  		 		$("#feedback").text("Very Hot");
+	  		 	}
+	  		 	else if (difference<=5 && difference >= -5 && difference !==0){
+	  		 		$("#feedback").text("Hot");
+	  		 	} else if (difference<=10 && difference >= -10 && difference !==0){
+	  		 		$("#feedback").text("Warm");
+	  		 	}
+	  		 	else if (difference<=25 && difference >= -25 && difference !==0){
+	  		 		$("#feedback").text("Luke warm");
+	  		 	}
+	  		 	 else if (value == numb){
 	  		 		  $("#feedback").text("Correct");
+	  		 	}
+	  		 	else {
+	  		 		$("#feedback").text("Cold");
 	  		 	}
 	  	}
 	  		else {
